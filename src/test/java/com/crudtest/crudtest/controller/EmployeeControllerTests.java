223deb519568aff763b1d63995f64f895a17ef7c
Tapiwanashe Shoshore <tapiwanashe.shoshore@sovtech.com>
2022-12-03 15:27:40 +0200

    add employee tests

---
+++ src/test/java/com/crudtest/crudtest/controller/EmployeeControllerTests.java
@@ -0,0 +1,56 @@
+package com.crudtest.crudtest.controller;
+
+import com.crudtest.crudtest.domain.Employee;
+import com.crudtest.crudtest.service.EmployeeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest
+public class EmployeeControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private EmployeeService employeeService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void createEmployeeShouldReturnCreatedEmployee() throws Exception {
+        //given
+        Employee employee = Employee.builder()
+                .firstName("Tapiwa")
+                .lastName("Shoshore")
+                .email("tapiwa@gmail.com")
+                .build();
+        given(employeeService.saveEmployee(any(Employee.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
+        //when
+        ResultActions resultActions = mockMvc.perform(post("/api/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employee)));
+
+        //then
+        resultActions.andDo(print()).
+                andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName",
+                        is(employee.getFirstName())))
+                .andExpect(jsonPath("$.lastName",
+                        is(employee.getLastName())))
+                .andExpect(jsonPath("$.email",
+                        is(employee.getEmail())));
+    }
+}
