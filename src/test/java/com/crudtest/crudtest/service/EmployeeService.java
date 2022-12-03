17a437f9c1ceedf6da02931ef82f121106e13aca
Tapiwanashe Shoshore <tapiwanashe.shoshore@sovtech.com>
2022-12-03 13:42:28 +0200

    create employee service

---
+++ src/test/java/com/crudtest/crudtest/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.crudtest.crudtest.service;
+
+import com.crudtest.crudtest.domain.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    Optional<Employee> getEmployeeById(long id);
+
+    Employee updateEmployee(Employee updatedEmployee);
+
+    void deleteEmployee(long id);
+}
